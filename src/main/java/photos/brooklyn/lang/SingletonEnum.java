be2f87ead49741c2088988e1abd854a65a46b44b
Brooklyn Photos <guoneng.list@gmail.com>
2020-01-10 23:24:03 +0100

    singleton using enum

---
+++ src/main/java/photos/brooklyn/lang/SingletonEnum.java
@@ -0,0 +1,23 @@
+package photos.brooklyn.lang;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingletonEnum {
+    public static enum Singleton {
+        INSTANCE;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final int size = 10;
+        CountDownLatch cl = new CountDownLatch(size);
+        ExecutorService svc = Executors.newFixedThreadPool(size);
+        for (int i = 0; i < 10; i++) {
+            svc.submit(() -> System.out.println(Singleton.INSTANCE));
+            cl.countDown();
+        }
+        cl.await();
+        svc.shutdown();
+    }
+}
